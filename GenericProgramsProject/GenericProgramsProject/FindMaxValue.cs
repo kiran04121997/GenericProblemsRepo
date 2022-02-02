803b2ab3b628bb9d92cc05b967fdbf41145766e9
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-02 23:15:41 +0530

    add the printmax

--- GenericProgramsProject/GenericProgramsProject/FindMaxValue.cs
+++ GenericProgramsProject/GenericProgramsProject/FindMaxValue.cs
@@ -6,41 +6,40 @@ using System.Threading.Tasks;
 
 namespace GenericProgramsProject
 {
-  
-        public class FindMaxValue<T> where T : IComparable
-        {
-            /* UC- 4 Implemented
-             * taking more then 3 parameters as in Array
-             * Sorting Array to get max Value
-             */
-            //Instance Variables
-            public T[] value;
-            //Constructor
-            public FindMaxValue(T[] value)
-            {
-                this.value = value;
-            }
-            //Generic Method to sort Array in asscending order
-            public T[] Sort(T[] values)
-            {
-                Array.Sort(values);
-                return values;
-            }
-            //Method to get Max value After Sorting.
-            public T MaxValue(params T[] values)
-            {
-                var sortedValues = Sort(values);
-                var s = sortedValues.Length - 1;
-                return sortedValues[s];
-            }
-            // Method to display Max Value
-            public void PrintMaxValue()
-            {
-                Console.WriteLine("Max Value is: " + MaxValue(this.value));
-            }
 
+    public class FindMaxValue<T> where T : IComparable
+    {
+        /* UC- 5 Implemented
+         * Using Generic Method to get Max Value.
+         */
+        //Instance Variables
+        public T[] value;
+        //Constructor
+        public FindMaxValue(T[] value)
+        {
+            this.value = value;
+        }
+        //Generic Method to sort Array in asscending order
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        //Method to get Max value After Sorting.
+        public T MaxValue(params T[] values)
+        {
+            var sortedValues = Sort(values);
+            var s = sortedValues.Length - 1;
+            return sortedValues[s];
+        }
+        //Using Generic Method to get Max value
+        public T PrintMaxValue()
+        {
+            T max = MaxValue(this.value);
+            return max;
         }
     }
+}
 
 
 
